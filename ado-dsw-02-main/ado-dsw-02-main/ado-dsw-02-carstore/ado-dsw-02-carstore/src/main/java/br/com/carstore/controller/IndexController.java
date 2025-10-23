eedad0b10ad50705bcc2b6c47d25f6e0500adcb6
Bruno Silva Costa <101366729+Bruninc@users.noreply.github.com>
2025-10-23 19:15:28 -0300

    Add files via upload

---
+++ ado-dsw-02-main/ado-dsw-02-main/ado-dsw-02-carstore/ado-dsw-02-carstore/src/main/java/br/com/carstore/controller/IndexController.java
@@ -0,0 +1,27 @@
+package br.com.carstore.controller;
+
+import br.com.carstore.dto.CarDTO;
+import br.com.carstore.model.Car;
+import br.com.carstore.model.CarResponseEntity;
+import br.com.carstore.service.CarService;
+import br.com.carstore.service.CarServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class IndexController {
+
+    CarService carService;
+
+    @PostMapping("/api/cars")
+    public ResponseEntity<CarDTO> createCar(@RequestBody CarDTO car) {
+        this.carService.save(car);
+        return ResponseEntity.ok().build();
+    }
+
+}
+
---
+++ ado-dsw-02-main/ado-dsw-02-main/ado-dsw-02-carstore/ado-dsw-02-carstore/src/main/java/br/com/carstore/service/CarServiceImpl.java
@@ -0,0 +1,40 @@
+package br.com.carstore.service;
+
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.dao.CarJpaDao;
+import br.com.carstore.dto.CarDTO;
+import jakarta.validation.OverridesAttribute;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService{
+
+    private final CarJpaDao carJpaDao;
+
+    public CarServiceImpl(CarJpaDao carJpaDao){
+        this.carJpaDao = carJpaDao;
+    }
+
+    @Override
+    public List<CarDTO> findAll() {
+        return carJpaDao.findAll();
+    }
+
+    @Override
+    public void save(CarDTO carDTO) {
+        carJpaDao.save(carDTO);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        carJpaDao.deleteById(id);
+    }
+
+    @Override
+    public void update(String id, CarDTO carDTO) {
+        carJpaDao.update(id, carDTO);
+    }
+}
